21ff77e517539b41e69c9d66b9a65d65f5ef45af
Ed Tsang <edwardt.tril@gmail.com>
2013-03-20 16:20:12 -0700

    add skelton

---
+++ src/mod_oauth2_spark.erl
@@ -0,0 +1,34 @@
+%%%----------------------------------------------------------------------
+%%% 
+%%% @author : Edward Tsang <>
+%%% @doc OAuth2 client to internal OAuth2 server
+%%% Created : 20 Mar 2013
+%%%----------------------------------------------------------------------
+%%% Copyright (c)
+%%%---
+
+-module(mod_oauth2_spark).
+-author('edward tsang').
+
+-ifdef(TEST).
+-include_lib("eunit/include/eunit.hrl").
+-include_lib("kernel/include/file.hrl").
+-endif.
+
+-export([parse/2, usage/2, usage/3, usage/4]).
+
+
+-ifdef(TEST).
+
+
+-endif.
+
+
+
+
+
+
+
+%%%%% Eunit Tests %%%%%%%%
+
+
